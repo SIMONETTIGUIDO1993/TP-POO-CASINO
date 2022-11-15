dd21a593d43d3aa6a293c75964a3be06c9aed3a1
irinamarzioni <imarzioni@devartis.com>
2022-11-15 11:40:57 -0300

    correccion

--- caraSeca.ts
+++ caraSeca.ts
@@ -1,61 +1,58 @@
 export class CaraSeca {
-  protected valorDeApuesta:number;
-  protected valorPremio:number;
-  protected caraElegida:string;
+  protected valorDeApuesta: number;
+  protected valorPremio: number;
+  protected caraElegida: string;
 
-    constructor(pValorDeApuesta:number,pValorPremio:number,pCaraElegida:string) {
-      this.valorDeApuesta = pValorDeApuesta;
-      this.valorPremio = pValorPremio;
-      this.caraElegida = pCaraElegida;
-    }
-    
-    getRandomInt(max:number):number { 
-        return Math.floor(Math.random() * max);
-      }
-    
-    setCaraElegida(pCaraElegida:string):void{
-        this.caraElegida = pCaraElegida;
-    }
-
-
-      
-    jugarCaraSeca(pCaraElegida:string):any {
-      let caraSeca = this.getRandomInt(2);
-      let ladoResultado = this.convertirStringAnumber(caraSeca);
-      pCaraElegida = this.caraElegida;
-      if (this.valorDeApuesta > 0){
-
-        if (ladoResultado === pCaraElegida) {
-          this.sumarPremio();
-          return `Usted gano en CaraSeca! el resultado fue ${ladoResultado}, Su saldo actual es $ ${this.valorDeApuesta}`
-        
-        }else {
-          this.restarPremio();
-          return `Usted Perdio en CaraSeca! el resultado fue ${ladoResultado}, Su saldo actual es $ ${this.valorDeApuesta}`
-        }
-      } return "Usted no tiene Suficiente dinero para seguir jugando al CaraSeca, si quiere seguir jugando ingrese mas dinero!"
-
-    }
-
-    convertirStringAnumber(pLadoResultado:number):string {
-      if (pLadoResultado ===0){
-        return "cara";
-      } else{
-        return "seca";
-      }
-
-    }
-    restarPremio():void{
-      this.valorDeApuesta = this.valorDeApuesta - 10;
-    }
-    sumarPremio():void{
-        this.valorDeApuesta = this.valorDeApuesta + this.valorPremio;
-    }
-    setValorDeApuesta(pValorDeApuesta:number):void{
-      this.valorDeApuesta = pValorDeApuesta;
-  }
-  public getValorDeApuesta():number {
-      return this.valorDeApuesta;
+  constructor(pValorDeApuesta: number, pValorPremio: number) {
+    this.valorDeApuesta = pValorDeApuesta;
+    this.valorPremio = pValorPremio;
   }
 
+  getRandomInt(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  setCaraElegida(pCaraElegida: string): void {
+    this.caraElegida = pCaraElegida;
+  }
+
+  jugarCaraSeca(pCaraElegida: string): string {
+    this.setCaraElegida(pCaraElegida);
+    let caraSeca = this.getRandomInt(2);
+    let ladoResultado = this.convertiNnumberAString(caraSeca);
+
+    if (this.valorDeApuesta > 0) {
+      if (ladoResultado === pCaraElegida) {
+        this.sumarPremio();
+        return `Usted gano en CaraSeca! el resultado fue ${ladoResultado}, Su saldo actual es $ ${this.valorDeApuesta}`;
+      } else {
+        this.restarPremio();
+        return `Usted Perdio en CaraSeca! el resultado fue ${ladoResultado}, Su saldo actual es $ ${this.valorDeApuesta}`;
+      }
+    }
+    return "Usted no tiene Suficiente dinero para seguir jugando al CaraSeca, si quiere seguir jugando ingrese mas dinero!";
+  }
+
+  convertiNnumberAString(pLadoResultado: number): string {
+    if (pLadoResultado === 0) {
+      return "cara";
+    } else {
+      return "seca";
+    }
+  }
+  restarPremio(): void {
+    this.valorDeApuesta = this.valorDeApuesta - 10;
+  }
+
+  sumarPremio(): void {
+    this.valorDeApuesta = this.valorDeApuesta + this.valorPremio;
+  }
+
+  setValorDeApuesta(pValorDeApuesta: number): void {
+    this.valorDeApuesta = pValorDeApuesta;
+  }
+
+  getValorDeApuesta(): number {
+    return this.valorDeApuesta;
+  }
 }
--- casino.ts
+++ casino.ts
@@ -5,33 +5,27 @@ import { Dado } from './Dado';
 import { Jugada } from './jugada'
 import * as fs from 'fs';
 
-
 let Bienvenidos: string = fs.readFileSync('bienvenidos.txt', 'utf8');
-let instruccionesTragamonedasComun: string = fs.readFileSync("instruccionesTragamonedasComun.txt",'utf8');
-let instruccionesTragamonedasBonus: string = fs.readFileSync("instruccionesTragamonedasBonus.txt",'utf8');
-let instruccionesCaraSeca: string = fs.readFileSync("instruccionesCaraSeca.txt",'utf8');
-let instruccionesDados: string = fs.readFileSync("instruccionesDados.txt",'utf8');
-
-
-
+let instruccionesTragamonedasComun: string = fs.readFileSync("instruccionesTragamonedasComun.txt", 'utf8');
+let instruccionesTragamonedasBonus: string = fs.readFileSync("instruccionesTragamonedasBonus.txt", 'utf8');
+let instruccionesCaraSeca: string = fs.readFileSync("instruccionesCaraSeca.txt", 'utf8');
+let instruccionesDados: string = fs.readFileSync("instruccionesDados.txt", 'utf8');
 
 console.log(Bienvenidos);
 
-
 //  Juego: tragamonedas
-let tragamonedas:Tragamonedas = new Tragamonedas(50,250);
+let tragamonedas: Tragamonedas = new Tragamonedas(50, 250, [1, 2, 3]);
 
 //  Juego:  tragamonedasBonus
-let tragamonedasBonus:TragamonedasBonus = new TragamonedasBonus(50,5);
+let tragamonedasBonus: TragamonedasBonus = new TragamonedasBonus(50, 250, [1, 2, 3]);
 
 // Juego: caraSeca
-let caraSeca: CaraSeca = new CaraSeca(50,10,"cara");
+let caraSeca: CaraSeca = new CaraSeca(50, 10);
 
 // Juego: dado
-let dados : Dado = new Dado(6,50,10);
+let dados: Dado = new Dado(6, 50, 10);
 
-
-let jugadas : Jugada = new Jugada(tragamonedas,tragamonedasBonus,caraSeca,dados); 
+let jugadas: Jugada = new Jugada(tragamonedas, tragamonedasBonus, caraSeca, dados);
 
 
 console.log(instruccionesTragamonedasComun);
@@ -39,9 +33,8 @@ console.log(instruccionesTragamonedasComun);
 // Si gana la jugada se le suma el valor previamente seteado de premio.
 // Si pierde la jugada se restan $10
 
-tragamonedas.setArregloCarretes([0,1,2]);
+tragamonedas.setArregloCarretes([0, 1, 2]);
 tragamonedas.setValorDeApuesta(30);
-
 
 let jugadaTragamonedas1 = jugadas.jugarJuego("tragamonedas");
 console.log(jugadaTragamonedas1);
@@ -56,9 +49,7 @@ let jugadaTragamonedas4 = jugadas.jugarJuego("tragamonedas");
 console.log(jugadaTragamonedas4);
 
 
-
 console.log(instruccionesTragamonedasBonus);
-
 // Para jugar al TragamonedasBonus hay que setear el valorDeApuesta
 // Se gana si los 3 numeros son iguales.
 // Si gana la jugada se suman $50
@@ -78,9 +69,7 @@ let jugadaTragamonedasBonus4 = jugadas.jugarJuego("tragamonedasBonus");
 console.log(jugadaTragamonedasBonus4);
 
 
-
 console.log(instruccionesCaraSeca);
-
 // Para jugar al CaraSeca hay que setear el valorDeApuesta y la cara elegida ( "cara" o "seca" en minuscula)
 // Si gana la jugada se le suma el valor previamente seteado de premio.
 // Si pierde la jugada se restan $10
@@ -94,9 +83,7 @@ let jugadaCaraSeca2 = jugadas.jugarJuego("caraSeca");
 console.log(jugadaCaraSeca2);
 
 
-
 console.log(instruccionesDados);
-
 // Para jugar al los Dados hay que setear el valorDeApuesta
 // Si el resultado da 7 u 11 gana la jugada y se suma el valor de premio seteado previamente.
 // Si pierde la jugada se restan $10
@@ -107,7 +94,3 @@ console.log(jugadaDado1);
 
 let jugadaDado2 = jugadas.jugarJuego("dado")
 console.log(jugadaDado2);
-
-
-
-
